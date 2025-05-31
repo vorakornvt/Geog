b42a258996e6eedc28083ff09f83abb520a7a4a6
vorakornvt <vorakorn.vt@gmail.com>
2025-06-01 00:54:43 +1000

    add

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,7 +8,16 @@ function App({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <link rel="shortcut icon" href="/public/assets/logoO.png" />
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="GREG is a fast, modern web app that provides detailed country information using the REST Countries API. Explore population, region, flags, and more in one place."
+        />
+        <meta
+          name="keywords"
+          content="GREG, country information app, REST Countries API, country data, world countries, population, geography, flags, modern web app"
+        />
+        <meta name="author" content="Vorakorn Taweetawon" />
       </Head>
       <Layout>
         <Component {...pageProps} />
